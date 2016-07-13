0630f1b2255d842fd21e52f45007879bdfef6653
Steve Meyer <stevemeyer@ip-89-254.wireless.oberlin.edu>
2016-07-13 09:18:55 -0400

    Pushing before trying this out on the server.

--- app.js
+++ app.js
@@ -47,104 +47,104 @@ var data = {
             "meter_two": "Some_meter2",
         },
     ],
-    buildings: [{
-            name: "Kahn",
-            orb_one: "Kahn first floor",
-            orb_two: "Kahn second floor"
-        },
-
-        {
-            name: "Barrows",
-            orb_one: "Barrows first floor",
-            orb_two: "Barrows second floor"
-        },
-    ]
+    buildings: ["Kahn", "Barrows"]
 };
 
 var settings = fs.readFileSync('./config.json');
-  try {
+try {
     settings = JSON.parse(settings);
     data.settings = settings;
-  }
-  catch (err) {
+} catch (err) {
     console.log('There has been an error parsing your JSON.');
     console.log(err);
-  }
+}
 
-var save_orb = function(params) {
+var save_orb = function(params, response) {
+    data.orbs.push(params);
     con.query('insert into oberlin_orbs (name, meter_one, meter_two) values ("' + params.name + '", "' + params.meter_one + '", "' + params.meter_two + '")',
         function(err, results) {
             if (err) throw err;
+            response.redirect('back');
         });
 };
 
-var save_building = function(params) {
-    con.query('insert into oberlin_buildings (name, orb_one, orb_two) values ("' + params.name + '", "' + params.orb_one + '", "' + params.orb_two + '")',
-        function(err, results) {
-            if (err) throw err;
-        });
-};
-
-var delete_orb = function(params) {
+var delete_orb = function(params, response) {
+    for (var i = 0; i < data.orbs.length; i++) {
+        if (data.orbs[i].name === params.delete) {
+            data.orbs.splice(i, 1);
+        }
+    }
     con.query('DELETE FROM oberlin_orbs WHERE name= ?', params.delete,
         function(err, result) {
             if (err) throw err;
         });
 };
 
-var delete_building = function(params) {
-    con.query('DELETE FROM oberlin_buildings WHERE name= ?', params.delete,
-        function(err, result) {
-            if (err) throw err;
-        });
+var building_picked = function(params, response) {
+
+
 };
 
-var save_settings = function(params){
-    var set = {"orbs":false, "buildings":false, "floors":false};
-    if(params.orbs !== undefined)
+
+var save_settings = function(params) {
+    var set = {
+        "orbs": false,
+        "buildings": false,
+        "floors": false
+    };
+    if (params.orbs !== undefined)
         set.orbs = true;
-    if(params.buildings !== undefined)
+    if (params.buildings !== undefined)
         set.buildings = true;
-    if(params.floors !== undefined)
+    if (params.floors !== undefined)
         set.floors = true;
     data.settings = set;
 };
 //----------Routing----------//
 app.get('/', (request, response) => {
-    // TODO: MAKE THIS A CALLBACK
-    refresh_orbs();
     response.render('home', data);
 });
 
-// TODO: SORT THIS OUT
 app.post('/', (request, response) => {
-    if (request.body.delete !== undefined) {
-        delete_orb(request.body);
-        delete_building(request.body);
-    }
-    if (request.body.orb_one !== undefined)
-        save_building(request.body);
-    if (request.body.meter_one !== undefined)
-        save_orb(request.body);
-    if (request.body.settings !== undefined)
-        save_settings(request.body);
+});
 
-    con.query('SELECT * FROM oberlin_orbs', function(err, results) {
-        if (err) throw err;
-        var temp = JSON.stringify(results);
-        data.orbs = JSON.parse(temp);
-    });
-    con.query('SELECT * FROM oberlin_buildings', function(err, results) {
-        if (err) throw err;
-        var temp = JSON.stringify(results);
-        data.buildings = JSON.parse(temp);
-        response.redirect('back');
-    });
+app.post('/save_orb', (request, response) => {
+    save_orb(request.body, response);
+});
+
+app.post('/delete_orb', (request, response) => {
+    delete_orb(request.body, response);
+});
+
+app.post('/building_picked', (request, response) => {
+    building_picked(request.body, response);
 });
 
 app.get('/test', (request, response) => {
-    response.render('test', settings);
+    response.render('test');
 });
+
+app.get('/auth', function (req, res) {
+    var querystring = require("querystring");
+    var result = querystring.stringify({client_id: "b1fa18a42a9b5fb0a27cb525d3283066aebf86284e6b08640faccafc133336f0",scope: "remote_control:all",state:"3(#0/!~",response_type:"code"});
+    console.log(result);
+    res.redirect("https://cloud.lifx.com/oauth/authorize?"+result);
+});
+
+// app.get('/redirect', function (req, res) {
+//   var code = req.query.code;
+//   console.log(code);
+//   oauth2.authCode.getToken({
+//     code: code,
+//     redirect_uri: 'http://environmentalorb.org/redirect'
+//   }, saveToken);
+//
+//   function saveToken(error, result) {
+//     if (error) { console.log('Access Token Error', error.message); }
+//     token = oauth2.accessToken.create(result);
+//   }
+// });
+
 //--------------------MySQL--------------------//
 var con = mysql.createConnection({
     host: "localhost",
@@ -162,6 +162,21 @@ con.connect(function(err) {
     console.log('Connection established');
 });
 
+con.query('SELECT * FROM oberlin_orbs', function(err, results) {
+    if (err) throw err;
+    var temp = JSON.stringify(results);
+    data.orbs = JSON.parse(temp);
+});
+con.query('SELECT * FROM oberlin_meters', function(err, results) {
+    if (err) throw err;
+    var temp = JSON.stringify(results);
+    data.meters = JSON.parse(temp);
+});
+con.query('SELECT name FROM oberlin_buildings', function(err, results) {
+    if (err) throw err;
+    var temp = JSON.stringify(results);
+    data.buildings = JSON.parse(temp);
+});
 refresh_orbs = function() {
     con.query('SELECT * FROM oberlin_orbs', function(err, results) {
         if (err) throw err;
@@ -169,6 +184,27 @@ refresh_orbs = function() {
         data.orbs = JSON.parse(temp);
     });
 };
+//--------------------OAuth--------------------//
+
+client_id = 'b1fa18a42a9b5fb0a27cb525d3283066aebf86284e6b08640faccafc133336f0';
+client_secret = '45daebf2070721410815e0b2e054d16c195bac1c1cdfa5e2ad438d3b35888af5';
+authorize_url = 'https://cloud.lifx.com/oauth/authorize?client_id=b1fa18a42a9b5fb0a27cb525d3283066aebf86284e6b08640faccafc133336f0&scope=remote_control:all&state=3(#0/!~&response_type=code';
+token_url = 'https://cloud.lifx.com/oauth/token';
+required_scope = 'remote_control:all';
+
+var oauth2 = require('simple-oauth2')({
+  clientID: client_id,
+  clientSecret: client_secret,
+  site: 'https://cloud.lifx.com/',
+  tokenPath: '/oauth/token',
+  authorizationPath: '/oauth/authorize'
+});
+
+var authorization_uri = oauth2.authCode.authorizeURL({
+  redirect_uri: 'http://environmentalorb.org/redirect',
+  scope: required_scope,
+  state: '3(#0/!~'
+});
 
 //con.end(function(err) {});
 
